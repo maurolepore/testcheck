f4e23671f525fa30d3ca73ee26cf859ba1f6d27c
Mauro Lepore <maurolepore@gmail.com>
2021-12-16 09:00:11 -0300

    Report findings

--- tests/testthat/_snaps/snapshots.md
+++ tests/testthat/_snaps/snapshots.md
@@ -1,5 +1,5 @@
 # snapshots 'hi'
 
     Code
-      # hi
+      # y
 
--- tests/testthat/test-snapshots.R
+++ tests/testthat/test-snapshots.R
@@ -1,3 +1,18 @@
 test_that("snapshots 'hi'", {
-  expect_snapshot("hi")
+  envvar <- Sys.getenv("ABC")
+  skip_if_not(envvar == "TRUE")
+
+  expect_snapshot("y")
 })
+
+# * Unseting envvar skips test()
+# * Unseting envvar skips check()
+
+# * Setting envvar in .Renviron runs the snapshot with test()
+# * Setting envvar in .Renviron runs the snapshot with check()
+
+# * Setting envvar in ~/.Renviron runs the snapshot with test(): NO
+# * Setting envvar in ~/.Renviron runs the snapshot with check(): NO
+
+# * Unsetting envvar with snapshot.new.md fails with test()
+# * Unsetting envvar with snapshot.new.md fails with check()
